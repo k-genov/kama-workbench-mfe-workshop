b87bd3c28b948ac4ac2f82fc3cfd829f1af95eb8
k-genov <konstantin.genov@gmail.com>
2026-01-09 17:06:34 +0100

    7. title resolver

--- projects/client/src/app/app.routes.ts
+++ projects/client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {path: 'activator', loadChildren: () => import('./activator/activator.module')},
   {path: 'desktop', loadComponent: () => import('./desktop/desktop')},
   {path: 'projects', loadComponent: () => import('./projects/projects')},
   {path: 'project', loadComponent: () => import('./project/project')},
