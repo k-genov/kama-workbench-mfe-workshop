6cec461f6c2c15d22d62c3ef3489538df76ae3d9
k-genov <konstantin.genov@gmail.com>
2026-01-09 17:14:47 +0100

    8. message box

--- projects/client/src/app/activator/activator.module.ts
+++ projects/client/src/app/activator/activator.module.ts
@@ -1,10 +1,17 @@
 import {inject, NgModule} from '@angular/core';
 import {ProjectTitleResolver} from './project-title.resolver';
+import {WorkbenchClient} from '@scion/workbench-client';
 
 @NgModule({})
 export default class ActivatorModule {
 
   constructor() {
-    inject(ProjectTitleResolver);
+    inject(ProjectTitleResolver)
+    WorkbenchClient.registerTextProvider((key, params) => translations.get(key));
   }
 }
+
+const translations = new Map<string, string>()
+  .set('yes.action', 'Yes')
+  .set('no.action', 'No')
+  .set('delete.message', 'Do you want to delete?');
